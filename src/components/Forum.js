ae5799a22d380f07a27293fe473e848f59cb0b1c
Myanhtle <mtl2mr@virginia.edu>
2021-06-07 12:44:07 -0400

    setup basic forum post

---
+++ src/components/Forum.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ForumPost from "./ForumPost";
+
+export default function Forum() {
+  return (
+    <div>
+      <p>forum</p>
+      <ForumPost />
+    </div>
+  );
+}
