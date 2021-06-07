af69029a113d9813768e85c8e84dd6f77f363a7d
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-06-07 11:52:04 -0400

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={null} exact />
+        <Route path="/about" component={null} />
+        <Route path="/blog" component={null} />
+        <Route path="/forum" component={null} />
+        <Route path="/login" component={null} />
+        <Route component={Error} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
