93e2da53059944a7c0049c12c69e0dcfdc32791c
Myanhtle <mtl2mr@virginia.edu>
2021-06-07 16:27:29 -0400

    added another input field for title of post

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ErrorPage from "./components/ErrorPage";
 import NavBar from "./components/NavBar";
-import Forum from "./components/Forum";
+import Forum from "./components/forum/Forum";
 import ColorExamples from "./components/ColorExamples";
 import DataForm from "./components/dataForm";
 
