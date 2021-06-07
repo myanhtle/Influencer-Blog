d19be143ba6f75a9677c2c4fbfa29ed8f88726d3
aer6xrk <71356348+aer6xrk@users.noreply.github.com>
2021-06-07 16:26:17 -0400

    built out blog.js server-side with get and add functionality

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import DataForm from "./components/dataForm";
 import React from "react";
 import Forum from "./components/Forum";
+import TestBlog from "./components/testBlog";
 
 function App() {
   return (
     <div className="App">
       <DataForm />
+      <TestBlog />
     </div>
   );
 }
