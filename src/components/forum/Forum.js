75ea81165438d4690088d17c07f69bfcc13a182b
Myanhtle <mtl2mr@virginia.edu>
2021-06-08 12:48:49 -0400

    can add posts to the forum and if fields are not filled out, a warning pops up

--- src/components/forum/Forum.js
+++ src/components/forum/Forum.js
@@ -4,17 +4,22 @@ import CreateForumPost from "./CreateForumPost";
 
 export default function Forum() {
   const [posts, setPosts] = useState([]);
+  const [clickedPost, setClickedPost] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:8080/forum/read")
       .then((res) => res.json())
       .then((data) => setPosts(data))
       .then(console.log(posts));
-  }, []);
+  }, [clickedPost]);
 
   return (
     <div>
-      <CreateForumPost />
+      <CreateForumPost
+        posts={posts}
+        setPosts={setPosts}
+        setClickedPost={setClickedPost}
+      />
       {posts.map((p) => (
         <ForumPost p={p} posts={posts} setPosts={setPosts} />
       ))}
