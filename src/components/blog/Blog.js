5009e37e8c03eb607940237a8426aad7035350e0
Yaseen Bhuiyan <ywb9znu@virginia.edu>
2021-06-08 16:59:24 -0400

    working add and delete buttons

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -10,9 +10,17 @@ export default function Blog() {
     fetch(`http://localhost:8080/blog/read`)
       .then((res) => res.json())
       .then((data) => {
+        data.sort((a, b) => {
+          let da = a.date; //to order posts so
+          let db = b.date; //recent ones are first
+          if (da < db) return 1;
+          if (da > db) return -1;
+          return 0;
+        });
+        console.log(data);
         setBlog(data);
       });
-    console.log(blog);
+    setClicked(false);
   }, [clicked]);
 
   function handleClick() {
@@ -23,7 +31,6 @@ export default function Blog() {
       messageContent:
         "Couldn't resist my sugar tooth this morning. I had chocolate icecream for breakfast",
     };
-
     fetch("http://localhost:8080/blog/add", {
       method: "POST",
       headers: {
@@ -31,6 +38,21 @@ export default function Blog() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(postData),
+    });
+    setClicked(true);
+  }
+
+  function handleClickTwo(id) {
+    const postId = {
+      id,
+    };
+    fetch("http://localhost:8080/blog/delete", {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postId),
     });
   }
 
@@ -41,20 +63,54 @@ export default function Blog() {
         <div
           style={{ marginLeft: "35%", marginRight: "35%", textAlign: "center" }}
         >
-          <Button onClick={handleClick} variant="contained" color="primary">
+          <Button
+            onClick={() => {
+              handleClick();
+              setClicked(true);
+            }}
+            variant="contained"
+            color="primary"
+          >
             Create New Post
           </Button>
         </div>
         {blog.map((b) => (
           <Card className="card">
             <CardContent>
-              <Typography variant="h6">{b.title}</Typography>
+              <Typography variant="h6">
+                {b.title}
+                <div
+                  style={{
+                    float: "right",
+                    marginLeft: "auto",
+                    marginRight: "0",
+                  }}
+                >
+                  <Button variant="contained" color="secondary">
+                    edit
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      handleClickTwo(b.id);
+                      setClicked(true);
+                    }}
+                    variant="contained"
+                    color="secondary"
+                  >
+                    delete
+                  </Button>
+                </div>
+              </Typography>
               <Typography variant="subtitle2">{b.date}</Typography>
               <br></br>
-              <div className="fakeimg" style={{ height: "200px" }}>
-                Image
-              </div>
-              <br></br>
+              {b.image && (
+                <div>
+                  <div className="img" style={{ height: "200px" }}>
+                    Image
+                  </div>
+                  <br></br>
+                </div>
+              )}
               <Typography variant="body2">{b.messageContent}</Typography>
             </CardContent>
           </Card>
