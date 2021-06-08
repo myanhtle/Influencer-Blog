2645febb700b91d72edbd9dd1fdb5f56affd4a57
aer6xrk <71356348+aer6xrk@users.noreply.github.com>
2021-06-08 09:02:28 -0400

    added delete, update, like, and unlike functionality to blog.js. Also recreated serviceAccountKey.

--- src/components/testBlog.js
+++ src/components/testBlog.js
@@ -12,24 +12,73 @@ function TestBlog() {
   //   console.log(blog);
   // }, [clicked]);
 
-  function handleClick() {
-    const postData = {
-      date: "2021-06-07",
-      likes: 0,
-      messageContent: "Official blog is under construction!",
+  // function handleClick() {
+  //   const postData = {
+  //     date: "2021-06-07",
+  //     likes: 0,
+  //     messageContent: "Official blog is under construction!",
+  //   };
+
+  //   fetch("http://localhost:8080/blog/add", {
+  //     method: "POST",
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(postData),
+  //   });
+  // }
+
+  // function handleClickTwo() {
+  //   const postId = {
+  //     id: "LijDaywEwDlFpWAJCIHU",
+  //   };
+
+  //   fetch("http://localhost:8080/blog/delete", {
+  //     method: "DELETE",
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(postId),
+  //   });
+  // }
+
+  // function handleClickThree() {
+  //   const updatedPost = {
+  //     date: "2021-06-14",
+  //     messageContent:
+  //       "Official blog under construction, woohoo! Tune in for the finished product.",
+  //     id: "rlhVQQNxYIwozARZlCJT",
+  //   };
+  //   if (updatedPost)
+  //     fetch("http://localhost:8080/blog/update", {
+  //       method: "POST",
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(updatedPost),
+  //     });
+  // }
+
+  function handleClickFour() {
+    const updatedLikes = {
+      id: "rlhVQQNxYIwozARZlCJT",
+      currentLikeCount: 2,
     };
 
-    fetch("http://localhost:8080/blog/add", {
+    fetch("http://localhost:8080/blog/like", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(postData),
+      body: JSON.stringify(updatedLikes),
     });
   }
 
-  return <Button onClick={() => handleClick()}>Hello</Button>;
+  return <Button onClick={() => handleClickFour()}>Hello</Button>;
 }
 
 export default TestBlog;
