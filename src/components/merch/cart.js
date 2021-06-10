8c5e250465ef13f7f068c2cfd7c10f52e01ef0ad
aer6xrk <71356348+aer6xrk@users.noreply.github.com>
2021-06-10 17:24:37 -0400

    detail changes to cart back and front end

--- src/components/merch/cart.js
+++ src/components/merch/cart.js
@@ -1,3 +1,39 @@
+import CheckoutForm from "../CheckoutForm";
+import CartCard from "./cartCard";
+import { useContext, useState, useEffect } from "react";
+import { UserContext } from "../../contexts/UserContext";
+
 export default function Cart() {
-  return <h1>HIIIIII</h1>;
+  const { username } = useContext(UserContext);
+  const [sum, setSum] = useState();
+  const [cart, setCart] = useState([]);
+
+  console.log(username);
+
+  const fetchSum = () => {
+    fetch(`http://localhost:8080/cart/sum/${username}`)
+      .then((res) => res.json())
+      .then((data) => setSum(data));
+  };
+
+  useEffect(() => {
+    fetchSum();
+    fetch(`http://localhost:8080/cart/read/${username}`)
+      .then((res) => res.json())
+      .then((data) => setCart(data));
+  }, [username]);
+
+  console.log(sum);
+
+  console.log(cart);
+
+  return (
+    <>
+      {cart.map((item) => (
+        <div>
+          <CartCard item={item} />
+        </div>
+      ))}
+    </>
+  );
 }
