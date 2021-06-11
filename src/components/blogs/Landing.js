4f78b3ddb1c6cdb5d6bfb01e1fd775b3cea35a0e
Yaseen Bhuiyan <ywb9znu@virginia.edu>
2021-06-10 21:06:28 -0400

    Update Landing.js
    Added featured posts to blog landing page

--- src/components/blogs/Landing.js
+++ src/components/blogs/Landing.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Button,
@@ -8,6 +8,7 @@ import {
   List,
   ListItem,
   ListItemText,
+  Typography,
 } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import CreateNewButton from "./CreateButton";
@@ -68,6 +69,21 @@ export default function Landing() {
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
+
+  const [featured, setFeatured] = useState(null);
+  useEffect(() => {
+    fetch(`http://localhost:8080/blog/read`)
+      .then((res) => res.json())
+      .then((data) => {
+        let data2 = [];
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].featured) {
+            data2.push(data[i]);
+          }
+        }
+        setFeatured(data2);
+      });
+  }, []);
 
   return (
     <div style={{ textAlign: "center" }}>
@@ -140,9 +156,53 @@ export default function Landing() {
           <br></br>
           <br></br>
           <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
+          <h2>Featured Posts</h2>
+          <div
+            style={{
+              display: "flex",
+              marginLeft: "10%",
+              marginRight: "10%",
+            }}
+          >
+            {featured &&
+              featured.map((f) => (
+                <div
+                  style={{
+                    flexBasis: "50%",
+                    marginLeft: "5%",
+                    marginRight: "5%",
+                  }}
+                >
+                  <Card className="card">
+                    {f.image && (
+                      <div>
+                        <img className="img" src={f.image}></img>
+                        <br></br>
+                      </div>
+                    )}
+                    <CardContent>
+                      <Typography variant="h6">{f.title}</Typography>
+                      <Typography variant="subtitle2">
+                        {fetchBlogs.date}
+                      </Typography>
+                      <br></br>
+                      <Typography variant="body2">
+                        {f.messageContent}
+                      </Typography>
+                      <br></br>
+                      <Button
+                        href={`/blog/${f.id}`}
+                        variant="contained"
+                        color="secondary"
+                        style={{ color: "white" }}
+                      >
+                        View Post
+                      </Button>
+                    </CardContent>
+                  </Card>
+                </div>
+              ))}
+          </div>
           <br></br>
         </div>
       )}
